dd0dc9d44a1c2edc2ef25815f5e6af0ab7614fb7
evijung <evides01@gmail.com>
2016-11-08 17:39:12 +0700

    finished

--- app/src/main/java/com/th/samsen/tunyaporn/showpicturefromurl/MainActivity.java
+++ app/src/main/java/com/th/samsen/tunyaporn/showpicturefromurl/MainActivity.java
@@ -1,13 +1,58 @@
 package com.th.samsen.tunyaporn.showpicturefromurl;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        SynLoadImage synLoadImage = new SynLoadImage(imageView);
+        synLoadImage.execute();
     }
+
+    private class SynLoadImage extends AsyncTask<String, Void, Bitmap> {
+        ImageView view;
+
+        public SynLoadImage(ImageView view) {
+            this.view = view;
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            String urlString = "http://service.eternity.co.th/tmspxd/images/photos/user1.png";
+            Bitmap bitmap = null;
+            try {
+                InputStream inputStream = new java.net.URL(urlString).openStream();
+                bitmap = BitmapFactory.decodeStream(inputStream);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            view.setImageBitmap(bitmap);
+        }
+    }
+
 }
